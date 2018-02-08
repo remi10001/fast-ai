38e44b7436285000b899da80b308f141fd522731
Ubuntu <ubuntu@ip-10-0-0-13.us-west-2.compute.internal>
2018-02-08 00:03:17 +0000

    Last t2 instance commit

--- HTR/image_ocr.py
+++ HTR/image_ocr.py
@@ -12,7 +12,7 @@ from keras import backend as K
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers import Input, Dense, Activation
 from keras.layers import Reshape, Lambda
-#from keras.layers.merge import add, concatenate
+from keras.layers.merge import add, concatenate
 from keras.layers import Input, Embedding, Reshape, merge, LSTM, Bidirectional
 from keras.models import Model
 from keras.layers.recurrent import GRU
